f15f36760d867bb0fb5e211ba623ce70e0617b3f
PatriciaArnedo <paaarnedo@gmail.com>
2020-10-06 23:19:43 -0400

    school domain lab

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,36 @@
 # code here!
+
+class School
+
+    attr_accessor :roster
+
+    def initialize(roster)
+
+        @roster = {}
+
+    end
+
+    def add_student(student, grade)
+
+        if !@roster.has_key?(grade)
+            @roster[grade] = []
+            @roster[grade] << student
+        else
+            @roster[grade] << student
+        end
+    
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        result = {}
+        @roster.each do |k,v|
+            result[k] = v.sort
+        end
+        result
+    end
+
+end
